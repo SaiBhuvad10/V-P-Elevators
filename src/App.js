56f0f4cc03aa043b26c616e38fc0ec19625aff38
Sai Bhuvad <saibhuvad472@gmail.com>
2025-07-18 16:18:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1 className='mt-24 font-bold'>hello Tailwind</h1>
+      <p className='text-red-900 mx-48'>browser</p>
+    </div>
+  );
+}
+
+export default App;
