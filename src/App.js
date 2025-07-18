773b11b0511c60231c7e8e864e144f9fe8c63952
Sai Bhuvad <saibhuvad472@gmail.com>
2025-07-18 16:33:46 +0530

    Redo All

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ function App() {
     <div>
       <h1 className='mt-24 font-bold'>hello Tailwind</h1>
       <p className='text-red-900 mx-48'>browser</p>
-      <h1 className="text-5xl">Kolhapur</h1>
-      <h1 className="text-5xl">Kolhapur</h1>
     </div>
   );
 }
