a3beb05804e9e48e80c1ee41be367d7204ebe37d
Sai Bhuvad <saibhuvad472@gmail.com>
2025-08-01 00:54:25 +0530

    Add @vercel/speed-insights dependency and import in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
